d5202de4fd4f8550c1dae6a6816fc482430cda4b
Yury <k1networth23@gmail.com>
2025-03-13 06:50:41 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update -y
+sudo apt-get install -y kubelet kubeadm kubectl containerd
+sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo modprobe br_netfilter
+sudo modprobe overlay
+
+cat <<EOF | sudo tee -a /etc/sysctl.conf
+net.ipv4.ip_forward=1
+net.bridge.bridge-nf-call-iptables=1
+EOF
+sudo sysctl -p /etc/sysctl.conf
+
+# Configure containerd to use systemd as cgroup driver
+sudo mkdir -p /etc/containerd
+sudo tee /etc/containerd/config.toml > /dev/null <<EOF
+version = 2
+[plugins]
+  [plugins."io.containerd.grpc.v1.cri"]
+   [plugins."io.containerd.grpc.v1.cri".containerd]
+      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
+        [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+          runtime_type = "io.containerd.runc.v2"
+          [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+            SystemdCgroup = true
+EOF
+
+sudo systemctl restart containerd
+
+echo "Kubernetes components have been installed and configured successfully."
